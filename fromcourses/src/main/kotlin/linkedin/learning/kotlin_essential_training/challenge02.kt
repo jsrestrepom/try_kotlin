755067e1331b6e1788710c9c12dcba169a91d336
Santiago Restrepo Moncada <santyrestrepom@gmail.com>
2022-07-13 17:26:33 -0500

    Adding solution for challenge 02.

--- fromcourses/src/main/kotlin/linkedin/learning/kotlin_essential_training/challenge02.kt
+++ fromcourses/src/main/kotlin/linkedin/learning/kotlin_essential_training/challenge02.kt
@@ -3,4 +3,22 @@ package linkedin.learning.kotlin_essential_training
 fun main() {
     val animals =
         listOf("apple", "biscuit", "apple", "cat", "cat", "cat", "dog", "elephant", "fox", "goat", "elephant")
+
+    removeDuplicatesWithSet(animals)
+    removeDuplicatesOldFashionWay(animals)
+}
+
+fun removeDuplicatesWithSet(items: List<String>) {
+    val result = items.toSet()
+    println(result)
+}
+
+fun removeDuplicatesOldFashionWay(items: List<String>) {
+    val result = mutableListOf<String>()
+    for (item in items) {
+        if (!result.contains(item)) {
+            result.add(item)
+        }
+    }
+    println(result)
 }
