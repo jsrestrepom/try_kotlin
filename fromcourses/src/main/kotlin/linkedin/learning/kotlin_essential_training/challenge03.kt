0ee3018be554300ac6fdbf0c054a1caa0731b61b
Santiago Restrepo Moncada <santyrestrepom@gmail.com>
2022-07-14 11:24:50 -0500

    Adding solution for challenge 03.

--- fromcourses/src/main/kotlin/linkedin/learning/kotlin_essential_training/challenge03.kt
+++ fromcourses/src/main/kotlin/linkedin/learning/kotlin_essential_training/challenge03.kt
@@ -1,5 +1,18 @@
 package linkedin.learning.kotlin_essential_training
 
+import java.io.File
+
 fun main() {
-    val res = "./resources"
+    val path = "./src/main/resources"
+
+    val theFile = File("$path/foo.txt")
+    val entries = mutableListOf<Double>()
+    theFile.forEachLine {
+        val entryAsDouble = it.toDoubleOrNull()
+        if (entryAsDouble != null) {
+            entries.add(entryAsDouble)
+        }
+    }
+
+    entries.forEach { println(it) }
 }
