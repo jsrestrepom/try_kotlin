801db8130bceb2fb11b4d8f9952b3e47adbf7a69
Santiago Restrepo Moncada <santyrestrepom@gmail.com>
2022-07-15 10:52:39 -0500

    Adding solution for challenge 06.

--- fromcourses/src/main/kotlin/linkedin/learning/kotlin_essential_training/challenge06.kt
+++ fromcourses/src/main/kotlin/linkedin/learning/kotlin_essential_training/challenge06.kt
@@ -1,9 +1,33 @@
 package linkedin.learning.kotlin_essential_training
 
-import kotlin.coroutines.async
-import kotlin.coroutines.runBlocking
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 import java.io.File
+import kotlin.system.measureTimeMillis
+
+
+fun simpleReader() = readFile("the_whale.txt")
+
+fun asyncReader() = runBlocking {
+    val aDeferred: Deferred<List<String>> = async { readFile("the_whale.txt") }
+    aDeferred.await().forEach { println(it) }
+}
+
+fun readFile(fileName: String): List<String> {
+    val path = "./src/main/resources"
+    val file = File("$path/$fileName")
+    return file.readLines()
+}
 
 fun main() {
+    val timeForSimpleReader = measureTimeMillis {
+        simpleReader()
+    }
+    val timeForAsyncReader = measureTimeMillis {
+        asyncReader()
+    }
 
+    println("timeForSimpleReader: $timeForSimpleReader ms")
+    println("timeForAsyncReader: $timeForAsyncReader ms")
 }
