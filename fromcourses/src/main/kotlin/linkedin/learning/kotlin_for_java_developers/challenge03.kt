02a60954b1762119c3941c056a87a0995d04685d
Santiago Restrepo Moncada <santyrestrepom@gmail.com>
2022-07-15 17:34:59 -0500

    Adding solution for challenge 03.

--- fromcourses/src/main/kotlin/linkedin/learning/kotlin_for_java_developers/challenge03.kt
+++ fromcourses/src/main/kotlin/linkedin/learning/kotlin_for_java_developers/challenge03.kt
@@ -1,7 +1,23 @@
 package linkedin.learning.kotlin_for_java_developers
 
+class Cubic(val x: Int, val y: Int, val z: Int) {
+    operator fun plus(other: Cubic): Cubic {
+        return Cubic(x + other.x, y + other.y, z + other.z)
+    }
+
+    operator fun minus(other: Cubic): Cubic {
+        return Cubic(x - other.x, y - other.y, z - other.z)
+    }
+
+    override fun toString(): String {
+        return "($x, $y, $z)"
+    }
+}
+
+
 fun main() {
     println("operator")
+    println("----------")
 
     val pt1 = Cubic(100, 200, 300)
     val pt2 = Cubic(10, 20, 30)
