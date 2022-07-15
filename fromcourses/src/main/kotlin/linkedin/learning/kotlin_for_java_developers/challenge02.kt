6f4ba29021e4680927a28cba736206cd18a552fb
Santiago Restrepo Moncada <santyrestrepom@gmail.com>
2022-07-15 17:03:23 -0500

    Adding solution for challenge 02.

--- fromcourses/src/main/kotlin/linkedin/learning/kotlin_for_java_developers/challenge02.kt
+++ fromcourses/src/main/kotlin/linkedin/learning/kotlin_for_java_developers/challenge02.kt
@@ -1,7 +1,14 @@
 package linkedin.learning.kotlin_for_java_developers
 
+
+fun <T: Comparable<T>> getSmaller(param1: T, param2: T): T {
+    val result = param2.compareTo(param1)
+    return if (result < 0) param2 else param1
+}
+
 fun main() {
     println("getSmaller")
+    println("----------")
 
     val minInt: Int = getSmaller(42, 99)
     val minDouble: Double = getSmaller(9.887, 3.14159)
