a919e49919ae3e98114f90da7ad1e151a5dfd312
Santiago Restrepo Moncada <santyrestrepom@gmail.com>
2022-07-15 16:34:29 -0500

    Adding solution for challenge 01.

--- fromcourses/src/main/kotlin/linkedin/learning/kotlin_for_java_developers/challenge01.kt
+++ fromcourses/src/main/kotlin/linkedin/learning/kotlin_for_java_developers/challenge01.kt
@@ -1,5 +1,39 @@
 package linkedin.learning.kotlin_for_java_developers
 
+
+fun ifFizzBuzz() {
+    for (number in 1..105) {
+        val answer = if (number % 3 == 0 && number % 5 == 0) {
+            "fizz buzz"
+        } else if (number % 3 == 0) {
+            "fizz"
+        } else if (number % 5 == 0) {
+            "buzz"
+        } else {
+            ""
+        }
+        println("($number) $answer")
+    }
+}
+
+fun whenFizzBuzz() {
+    for (number in 1..105) {
+        val answer = when {
+            number % 3 == 0 && number % 5 == 0 -> "fizz buzz"
+            number % 3 == 0 -> "fizz"
+            number % 5 == 0 -> "buzz"
+            else -> ""
+        }
+        println("($number) $answer")
+    }
+}
+
 fun main() {
     println("Fizz Buzz")
+    println("----------")
+
+    ifFizzBuzz()
+    println("----------")
+    whenFizzBuzz()
+
 }
