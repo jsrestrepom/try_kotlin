e7baf0dc8d3933d47c7e6cb705689dd4661cdfa5
Santiago Restrepo Moncada <santyrestrepom@gmail.com>
2022-07-15 09:45:48 -0500

    Adding solution for challenge 04.

--- fromcourses/src/main/kotlin/linkedin/learning/kotlin_essential_training/challenge04.kt
+++ fromcourses/src/main/kotlin/linkedin/learning/kotlin_essential_training/challenge04.kt
@@ -5,5 +5,18 @@ fun replicate(func: (Int, String) -> Unit) {
 }
 
 fun main() {
+    val asLambdaExpression: (Int, String) -> Unit = { times: Int, message: String -> println(message.repeat(times)) }
+    replicate(asLambdaExpression)
 
+    replicate { times: Int, message: String -> println(message.repeat(times)) }
+
+    replicate(fun(times: Int, message: String) {
+        println(message.repeat(times))
+    })
+
+    replicate { count, msg ->
+        for (index in 1..count) {
+            println(msg)
+        }
+    }
 }
